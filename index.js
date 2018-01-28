54aee43f1dc068974d15cc6eeeec4c16c91a618a
kolombet <anonrab@ya.ru>
2018-01-29 01:55:50 +0300

    build

--- index.js
+++ index.js
@@ -2,6 +2,44 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 let app = express();
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+    // Pass to next layer of middleware
+    next();
+});
+// app.all('*', function(req, res,next) {
+//   /**
+//    * Response settings
+//    * @type {Object}
+//    */
+//   var responseSettings = {
+//       "AccessControlAllowOrigin": req.headers.origin,
+//       "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+//       "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+//       "AccessControlAllowCredentials": true
+//   };
+//   /**
+//    * Headers
+//    */
+//   res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials.toString());
+//   res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin.toString());
+//   res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'].toString() : "x-requested-with");
+//   res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'].toString() : responseSettings.AccessControlAllowMethods);
+//   if ('OPTIONS' == req.method) {
+//       res.send(200);
+//   }
+//   else {
+//       next();
+//   }
+// });
 let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -36,5 +74,5 @@ app.post("/sendPackage", (req, res) => {
     }
     res.send("bad data");
 });
-app.listen(3000);
+app.listen(8080);
 //# sourceMappingURL=index.js.map
